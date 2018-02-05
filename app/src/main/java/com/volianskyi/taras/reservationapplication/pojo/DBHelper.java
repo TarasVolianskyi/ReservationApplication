6b1f7f1a8e57b87a470786c5c6e03c77c9afe986
TarasVolianskyi <volianskyitv@gmail.com>
2018-02-06 00:10:52 +0200

    add to class DBHelper constructor, getter and setter

--- app/src/main/java/com/volianskyi/taras/reservationapplication/pojo/DBHelper.java
+++ app/src/main/java/com/volianskyi/taras/reservationapplication/pojo/DBHelper.java
@@ -5,4 +5,27 @@ package com.volianskyi.taras.reservationapplication.pojo;
  */
 
 public class DBHelper {
+    private String name;
+    private String lastName;
+
+    public DBHelper(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
