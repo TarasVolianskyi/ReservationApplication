628c3ab8c68fb291c0fe2e871ef4019a7548abf5
TarasVolianskyi <volianskyitv@gmail.com>
2018-02-20 00:01:44 +0200

    refactoring of class Dialog

--- app/src/main/java/com/volianskyi/taras/reservationapplication/fragments/Dialog.java
+++ app/src/main/java/com/volianskyi/taras/reservationapplication/fragments/Dialog.java
@@ -14,6 +14,14 @@ String login;
         this.login = login;
     }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
     public static Dialog getInstance() {
         return ourInstance;
     }
