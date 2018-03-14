2300eb7505a8ca45eae2f8953a72b64267a93a76
tarasvolianskyi <t.volianskyi@aboutcode.us>
2018-03-14 18:46:57 +0200

    refactoring of code

--- app/src/main/java/com/volianskyi/taras/reservationapplication/fragments/AboutCompany.java
+++ app/src/main/java/com/volianskyi/taras/reservationapplication/fragments/AboutCompany.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.volianskyi.taras.reservationapplication.R;
 
@@ -18,6 +19,7 @@ public class AboutCompany extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.about_company, container, false);
+        TextView textView = (TextView) view.findViewById(R.id.tvNameEmployeeView);
         return view;
     }
 }
