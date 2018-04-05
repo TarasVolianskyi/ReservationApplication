3c4d355373ed637488f9d265f6598b4f2efe8ea3
tarasvolianskyi <t.volianskyi@aboutcode.us>
2018-04-05 19:06:17 +0300

    refactor of section TimeForReservation

--- app/src/main/java/com/volianskyi/taras/reservationapplication/fragments/OnlineReservation.java
+++ app/src/main/java/com/volianskyi/taras/reservationapplication/fragments/OnlineReservation.java
@@ -8,17 +8,36 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.volianskyi.taras.reservationapplication.MainActivity;
 import com.volianskyi.taras.reservationapplication.R;
+
+import java.util.Date;
 
 /**
  * Created by tarasvolianskyi on 29.01.18.
  */
 
 public class OnlineReservation extends Fragment implements View.OnClickListener {
-    View view;
+    private View view;
+    Bundle bundle;
+    private Button btnEmployee;
+    private Button btnOption;
+    private Button btnDateAndTime;
+    private Button btnMakeReservation;
+    private TextView tvEmployeeChosed;
+    private TextView tvOptionChosed;
+    private TextView tvDateAndTimeChosed;
+    private String employeeChosed;
+    private String optionChosed;
+    private String dateAndTimeChosed = "";
+    Date date = new Date();
+
+
+
+    int res = 1;
 
     @Nullable
     @Override
@@ -29,20 +48,51 @@ public class OnlineReservation extends Fragment implements View.OnClickListener 
     }
 
     private void initView() {
-        Button btnEmployee = (Button) view.findViewById(R.id.btnEmployeeOnlineReservationLayout);
-        Button btnOption = (Button) view.findViewById(R.id.btnOptionOnlineReservationLayout);
-        Button btnDateAndTime = (Button) view.findViewById(R.id.btnDateAndTimeOnlineReservationLayout);
+        bundle = this.getArguments();
+        if (bundle != null) {
+            dateAndTimeChosed = bundle.getString(TimeForReservation.TAG_CHOOSED_FULL_TIME, "");
+
+        }
+
+        Toast.makeText(getContext(), dateAndTimeChosed, Toast.LENGTH_SHORT).show();
+        btnEmployee = (Button) view.findViewById(R.id.btn_employee_online_reservation_layout);
+        btnOption = (Button) view.findViewById(R.id.btnOptionOnlineReservationLayout);
+        btnDateAndTime = (Button) view.findViewById(R.id.btnDateAndTimeOnlineReservationLayout);
+        btnMakeReservation = (Button) view.findViewById(R.id.btnMakeReservationOnlineReservationLayout);
         btnEmployee.setOnClickListener(this);
         btnOption.setOnClickListener(this);
         btnDateAndTime.setOnClickListener(this);
+        btnMakeReservation.setOnClickListener(this);
+        btnMakeReservation.setText("rrrrrrrrr");
+
+        tvEmployeeChosed = (TextView) view.findViewById(R.id.tv_chosed_employee_online_reservation_layout);
+        tvOptionChosed = (TextView) view.findViewById(R.id.tv_chosed_option_online_reservation_layout);
+        tvDateAndTimeChosed = (TextView) view.findViewById(R.id.tv_chosed_date_and_time_online_reservation_layout);
+        tvDateAndTimeChosed.setText(dateAndTimeChosed);
+        if (res == 2) {
+            btnMakeReservation.setVisibility(View.VISIBLE);
+        }
+
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.btn_employee_online_reservation_layout:
+                Toast.makeText(getActivity(), "Click on next", Toast.LENGTH_SHORT).show();
+                ((MainActivity) getActivity()).goToCalendar();
+                break;
+            case R.id.btnOptionOnlineReservationLayout:
+                Toast.makeText(getActivity(), "Click on next", Toast.LENGTH_SHORT).show();
+                ((MainActivity) getActivity()).goToCalendar();
+                break;
             case R.id.btnDateAndTimeOnlineReservationLayout:
                 Toast.makeText(getActivity(), "Click on next", Toast.LENGTH_SHORT).show();
                 ((MainActivity) getActivity()).goToCalendar();
+                break;
+            case R.id.btnMakeReservationOnlineReservationLayout:
+                Toast.makeText(getActivity(), "Make reservation", Toast.LENGTH_SHORT).show();
+                // ((MainActivity) getActivity()).goToCalendar();
                 break;
         }
     }
--- app/src/main/java/com/volianskyi/taras/reservationapplication/fragments/TimeForReservation.java
+++ app/src/main/java/com/volianskyi/taras/reservationapplication/fragments/TimeForReservation.java
@@ -4,10 +4,14 @@ package com.volianskyi.taras.reservationapplication.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,21 +19,29 @@ import android.widget.Toast;
 import com.volianskyi.taras.reservationapplication.MainActivity;
 import com.volianskyi.taras.reservationapplication.R;
 
-public class TimeForReservation extends Fragment implements View.OnClickListener {
+public class TimeForReservation extends Fragment implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+
+    public static final String TAG_CHOOSED_FULL_TIME = "tagChoosedFullTime";
+
 
     View view;
+    OnlineReservation onlineReservation;
+    Bundle bundle;
     private int choosedDay;
     private int choosedMonth;
     private int choosedYear;
+
     TextView textViewAlreadyChoosedDate;
     TextView textViewAlreadyChoosedTime;
-    String textAlreadyChoosedDate;
-    String textAlreadyChoosedTime;
+
+    private String textAlreadyChoosedDate;
+    private String textAlreadyChoosedTime;
+    private String textAlreadyChoosedFullTime;
+
     TableRow tableRowMorn;
     TableRow tableRowDayFirst;
     TableRow tableRowDaySecond;
     TableRow tableRowEvening;
-
 
     private Button btnVisibityMorning;
     private Button btnVisibityDay;
@@ -55,12 +67,16 @@ public class TimeForReservation extends Fragment implements View.OnClickListener
     private Button btnTimeR4C3;
     private Button btnTimeR4C4;
 
+    private Switch switchVisibityMorning;
+    private Switch switchVisibityDay;
+    private Switch switchVisibityEvening;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.time_for_reservation_fragment, container, false);
 
-        Bundle bundle = this.getArguments();
+        bundle = this.getArguments();
         if (bundle != null) {
             choosedDay = bundle.getInt(CalendarForReservation.TAG_CHOOSED_DAY, 0);
             choosedMonth = bundle.getInt(CalendarForReservation.TAG_CHOOSED_MONTH, 0);
@@ -84,17 +100,28 @@ public class TimeForReservation extends Fragment implements View.OnClickListener
 
 
     private void visibilityButtonsView() {
-        btnVisibityMorning = (Button) view.findViewById(R.id.btn_vis_gone_morn_time_reservation_fragment);
-        btnVisibityDay = (Button) view.findViewById(R.id.btn_vis_gone_day_time_reservation_fragment);
-        btnVisibityEvening = (Button) view.findViewById(R.id.btn_vis_gone_even_time_reservation_fragment);
-        btnVisibityMorning.setOnClickListener(this);
-        btnVisibityDay.setOnClickListener(this);
-        btnVisibityEvening.setOnClickListener(this);
-
         tableRowMorn = (TableRow) view.findViewById(R.id.rowMorn);
         tableRowDayFirst = (TableRow) view.findViewById(R.id.rowDayFirst);
         tableRowDaySecond = (TableRow) view.findViewById(R.id.rowDaySecond);
         tableRowEvening = (TableRow) view.findViewById(R.id.rowDayEvening);
+
+        switchVisibityMorning = (Switch) view.findViewById(R.id.switch_vis_gone_morn_time_reservation_fragment);
+        switchVisibityDay = (Switch) view.findViewById(R.id.switch_vis_gone_day_time_reservation_fragment);
+        switchVisibityEvening = (Switch) view.findViewById(R.id.switch_vis_gone_evening_time_reservation_fragment);
+
+        switchVisibityMorning.setOnCheckedChangeListener(this);
+        switchVisibityDay.setOnCheckedChangeListener(this);
+        switchVisibityEvening.setOnCheckedChangeListener(this);
+
+       // changeVisibilityOfRows(tableRowMorn, !switchVisibityMorning.isChecked());
+               /*    if(switchVisibityMorning.isChecked()){
+                tableRowMorn.setVisibility(View.GONE);
+            }else {
+                tableRowMorn.setVisibility(View.VISIBLE);
+            }
+*/
+
+
     }
 
     private void morningSection() {
@@ -145,6 +172,7 @@ public class TimeForReservation extends Fragment implements View.OnClickListener
     public void onClick(View view) {
         switch (view.getId()) {
             //visibility buttons
+            /*
             case R.id.btn_vis_gone_morn_time_reservation_fragment:
                 Toast.makeText(getActivity(), "Click on morning visibility", Toast.LENGTH_SHORT).show();
                 changeVisibilityOfRows(tableRowMorn);
@@ -158,106 +186,129 @@ public class TimeForReservation extends Fragment implements View.OnClickListener
                 Toast.makeText(getActivity(), "Click on evening visibility", Toast.LENGTH_SHORT).show();
                 changeVisibilityOfRows(tableRowEvening);
                 break;
+                */
 
             //morning row
             case R.id.btn_time_r1c1_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR1C1.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
             case R.id.btn_time_r1c2_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR1C2.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
             case R.id.btn_time_r1c3_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR1C3.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
             case R.id.btn_time_r1c4_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR1C4.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
 
             //day first row
             case R.id.btn_time_r2c1_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR2C1.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
             case R.id.btn_time_r2c2_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR2C2.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
             case R.id.btn_time_r2c3_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR2C3.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
             case R.id.btn_time_r2c4_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR2C4.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
 
             //day second row
             case R.id.btn_time_r3c1_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR3C1.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
             case R.id.btn_time_r3c2_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR3C2.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
             case R.id.btn_time_r3c3_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR3C3.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
             case R.id.btn_time_r3c4_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR3C4.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
 
             //evening row
             case R.id.btn_time_r4c1_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR4C1.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
             case R.id.btn_time_r4c2_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR4C2.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
             case R.id.btn_time_r4c3_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR4C3.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
             case R.id.btn_time_r4c4_time_reservation_fragment:
                 textAlreadyChoosedTime = btnTimeR4C4.getText().toString();
-                Toast.makeText(getActivity(), textAlreadyChoosedTime, Toast.LENGTH_SHORT).show();
-                textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+                sendBundleDataToMain(textAlreadyChoosedTime);
                 break;
-
-
 
         }
     }
 
-    private void changeVisibilityOfRows(TableRow tableRowForMethod) {
-        if (tableRowForMethod.getVisibility() == View.VISIBLE) {
+    private void sendBundleDataToMain(String textAlreadyChoosedTime) {
+        textViewAlreadyChoosedTime.setText(textAlreadyChoosedTime);
+        textAlreadyChoosedFullTime = "Date - " + textAlreadyChoosedDate + "; Time - " + textAlreadyChoosedTime;
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onlineReservation = new OnlineReservation();
+        bundle = new Bundle();
+        bundle.putString(TAG_CHOOSED_FULL_TIME, textAlreadyChoosedFullTime);
+        onlineReservation.setArguments(bundle);
+
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.cv_calendar_for_reservation, onlineReservation, "timeForReservation");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    private void changeVisibilityOfRows(TableRow tableRowForMethod, Boolean booleanFromSwitch) {
+        if (booleanFromSwitch == true) {
             tableRowForMethod.setVisibility(View.GONE);
         } else {
             tableRowForMethod.setVisibility(View.VISIBLE);
         }
+    }
 
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean bool) {
+        if (!switchVisibityMorning.isChecked()) {
+            tableRowMorn.setVisibility(View.GONE);
+        } else if (switchVisibityMorning.isChecked()) {
+            tableRowMorn.setVisibility(View.VISIBLE);
+        }else if (!switchVisibityDay.isChecked()) {
+            tableRowDayFirst.setVisibility(View.GONE);
+            //tableRowDaySecond.setVisibility(View.GONE);
+        } else if (switchVisibityDay.isChecked()) {
+            tableRowDayFirst.setVisibility(View.VISIBLE);
+            //tableRowDaySecond.setVisibility(View.VISIBLE);
+        }if (!switchVisibityEvening.isChecked()) {
+            tableRowEvening.setVisibility(View.GONE);
+        } else if (switchVisibityEvening.isChecked()) {
+            tableRowEvening.setVisibility(View.VISIBLE);
+        }
     }
 }
